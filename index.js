d74be3bcad2abe16dcb1f4c69e98ed1a1d36ce8b
Marvin Valke <valkemarvin@gmail.com>
2021-11-09 17:46:09 +0100

    lab solved with Joana

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
 
-  get(pos) {}
+    this.items.sort((first, second) => {
+      if (first > second) {
+        return 1;
+      } else if (first < second) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
 
-  max() {}
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
+
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let max = this.length - 1;
+      return this.items[max];
+    }
+  }
+
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
   sum() {}
 
